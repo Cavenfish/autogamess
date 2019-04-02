b46c0f8f78b71af64771b36fd61ef09fe7b3c25e
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-02 17:50:40 -0400

    added make_xyz function

--- help.py
+++ help.py
@@ -5,13 +5,10 @@ The following functions are in this file:
 
     ctr_f: similar to computer find tool (control f)
 
-    ctr_f_all: similar to computer find all tool
-
     angle_between: finds the angle between two vectors using the dot product
     formula (u \dot v = |u| |v| cos\theta )
 
-    get_bond_data: finds the bond lengths and angles, returns a
-    DataFrame with these values
+    make_xzy: turns a list of xyz values into a float tuple of xyz 
 
 """
 import numpy as np
@@ -27,3 +24,9 @@ def angle_between(v1, v2):
     normv1 = v1 / np.linalg.norm(v1)
     normv2 = v2 / np.linalg.norm(v2)
     return np.arccos(np.dot(v1,v2) / (normv1 * normv2))
+
+def make_xzy(xyzlist):
+    r = (float(xyzlist[0]),
+         float(xyzlist[0]),
+         float(xyzlist[0]))
+    return r
