140d352997f76c23e81c52700f49b2a89411c65b
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-07 14:02:22 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autogamess",
+    version="0.0.1",
+    author="Brian C. Ferrari",
+    author_email="brianf1996@knights.ucf.edu",
+    description="This is a python module for automating Raman calculations using GAMESS(us).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cavenfish/autogamess",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
