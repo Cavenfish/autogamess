36c0f7b47361614d05dbee303bc0bf3f343672de
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-04-09 15:10:07 -0400

    added __all__; functions from help moved into here; commonly used import are imported here now

--- autogamess/__init__.py
+++ autogamess/__init__.py
@@ -9,7 +9,32 @@ Contains the following functions:
                 opt2hes
                 sort_logs
                 hes2raman
+                get_data
 
 
 Author: Brian C. Ferrari
 """
+import os
+import numpy as np
+import pandas as pd
+
+__all__ = ["bat_maker", "new_project", "opt2hes", "sort_logs",
+           "hes2raman", "get_data"]
+
+
+def ctr_f(find_this, look_here):
+    for line in look_here:
+        if find_this in line:
+            return look_here.index(line)
+    return -1
+
+def angle_between(v1, v2):
+    v1hat = v1 / np.linalg.norm(v1)
+    v2hat = v2 / np.linalg.norm(v2)
+    return np.arccos(np.clip(np.dot(v1hat, v2hat), -1.0, 1.0))
+
+def make_xzy(xyzlist):
+    r = (float(xyzlist[0]),
+         float(xyzlist[0]),
+         float(xyzlist[0]))
+    return r
