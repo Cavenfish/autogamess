3270b3eabecea33a111d31867c3a4c1301b08a24
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-05-28 14:55:01 -0400

    removed bat maker import

--- autogamess/__init__.py
+++ autogamess/__init__.py
@@ -4,7 +4,6 @@ optimization, hessian, raman, and VSCF input generation and output
 data collection.
 
 Contains the following functions:
-                bat_maker
                 new_project
                 opt2hes
                 sort_logs
@@ -15,13 +14,15 @@ Contains the following functions:
 
 Author: Brian C. Ferrari
 """
-from .config        import *              #Configuration (holds Globals)
+    #Configuration (holds Globals)
+from .config        import *
 
-from .bat_maker     import bat_maker      #Functions
+    #Functions
 from .new_project   import new_project
 from .opt2hes       import opt2hes
 from .sort_logs     import sort_logs
 from .hes2raman     import hes2raman
 from .input_builder import input_builder
 
-from .get_data      import *              #Multi-function Modules
+    #Multi-function Modules
+from .get_data      import *
