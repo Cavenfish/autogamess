4dcbf5d1a0f9059f8bdbc1a346c8f9cced70f62d
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-06-07 14:50:27 -0400

    calling input builder parameters were mixed up

--- autogamess/new_project.py
+++ autogamess/new_project.py
@@ -136,7 +136,7 @@ def new_project(maindir, csvfile, ebasis_dir, initial_coords_dict=None,
 
     #Run Input Builder function
     save_dir = maindir + 'inputs/'
-    input_builder(csvfile, save_dir, ebasis_dir,
+    input_builder(csvfile, ebasis_dir, save_dir,
                   initial_coords_dict, title.replace('/', '\n'))
 
 
