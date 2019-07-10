98c56626277b6f5349ddcd81d1714b3b1826a670
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-07-10 14:23:57 -0400

    Added comments and began integration of emsl and periodic elements packages

--- autogamess/input_builder.py
+++ autogamess/input_builder.py
@@ -2,27 +2,53 @@ from .config import *
 import pkg_resources
 from .molecule_dictionary import molecule_dictionary
 
-def input_builder(inputfile, ebasis_dir, save_dir, 
-                  initial_coords_dict=None, proj_title=' Your Title Goes Here\n'):
-    '''
-    This function build optimization input files.
-    '''
+def input_builder(inputfile, save_dir, initial_coords_dict=None,
+                  proj_title=' Your Title Goes Here\n'):
+    """
+    This function builds optimization input files.
+
+    Parameters
+    ----------
+
+
+    Returns
+    ----------
+    This function returns nothing
+
+    Notes
+    ----------
+    This function uses the EMSL Basis Set Exchange module to import
+    external basis sets[1]. This function also uses the Periodic_Elements
+    package by VaasuDevanS [2].
+
+    [1] https://github.com/MolSSI-BSE/basis_set_exchange
+    [2] https://github.com/VaasuDevanS/Periodic_Elements
+
+
+    Examples
+    ----------
+    """
+    #Variable Declrations
     _       = '_'
     version = (' AutoGAMESS Version ' +
                 str(pkg_resources.require("autogamess")[0].version) )
     ibv     = 'AGv' + version.split(' ')[-1].replace('.', '-')
     opt     = '_opt.inp'
 
+    #Checks if initial_coords_dict is given
     if initial_coords_dict is None:
         initial_coords_dict = molecule_dictionary
 
+    #Read in input file
     df = pd.read_csv(inputfile)
 
+    #Get list of species/theories/basis sets
     species     = list(df['Species'].dropna())
     theories    = list(df['Theory'].dropna())
     basis_sets  = list(df['Basis Sets'].dropna())
     ebasis_sets = list(df['External Basis Sets'].dropna())
 
+    #Make list of file names
     files = []
     for specie in species:
         for theorie in theories:
@@ -35,52 +61,49 @@ def input_builder(inputfile, ebasis_dir, save_dir,
                               theorie.strip('\n') + _ +
                               basis.strip('\n') + opt )
 
-
-
-
-
+    #Make input files for GAMESS(us)
     for filename in files:
+        #Open file, write preamble
         f = open(save_dir + filename, 'w')
         f.write('!'+ version +'\n')
         f.write('!  by Brian C. Ferrari \n')
         f.write('!\n')
 
+        #Get index of 'theory' to replace with proper command
         i               = ctr_f('theory', basic_params)
+
+        #Get proper theory input style from dictionary
         theo            = theory_dict[filename.split(_)[2]]
+
+        #Get list version of tuple basic parameters
+        #(its a tuple to protect it from changes)
         params          = list(basic_params)
+
+        #Replace 'theory' with proper input parameter
         params[i]       = basic_params[i].replace('theory', theo)
 
+        #Fill in basis parameter for GAMESS(us) internal basis sets
         if filename.split(_)[3] in basis_sets:
             i               = ctr_f('=basis', basic_params)
             bset            = filename.split(_)[3]
             params[i]       = basic_params[i].replace('basis', bset)
+        #If not internal delete basis line from input file
         else:
             i               = ctr_f('=basis', basic_params)
             del params[i]
 
-
+        #Write parameters, and project title
         f.writelines(params)
         f.write(proj_title)
 
+        #Get initial coordinates from initial_coords_dict
         coords = initial_coords_dict[filename.split(_)[1]]
 
+        #Useing EMSL put in external basis sets
         if filename.split(_)[3] in ebasis_sets:
-            f2   = open(ebasis_dir + filename.split(_)[3] + '.txt', 'r')
-            temp = f2.readlines()
-            f2.close()
+            basis_name = filename.split(_)[3]
+            molecule   = filename.split(_)[1]
+            atoms      = [x for x in molecule if not x.isdigit()]
 
-            for coord in coords:
-                find    = coord.split('  ')[0]
-                i       = ctr_f(find, temp)
-                if i is -1:
-                    continue
-                temp[i] = coord
 
-            f.writelines(coords[0:2])
-            f.writelines(temp)
-
-        else:
-            f.writelines(coords)
-
-        f.write(' $END')
-        f.close()
+    return
