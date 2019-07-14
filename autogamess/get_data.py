8f36d170d90d86653ff07c9d5fb29d01646143f4
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-07-14 19:26:35 -0400

    Now gets data and puts into data object

--- autogamess/get_data.py
+++ autogamess/get_data.py
@@ -1,5 +1,32 @@
-import cclib
+#import cclib
 from .config import *
-from .get_data import *
+from .data_finder import *
 
-def get_data():
+class DATA:
+    def __init__(self):
+        return
+
+def get_data(filename):
+    #parser = cclib.io.ccopen(filename)
+    #data   = parser.parse()
+    data    = DATA()
+
+    if '_opt' in filename:
+        setattr(data, 'bond_lengths', optimization(filename)[0])
+        setattr(data,  'bond_angles', optimization(filename)[1])
+
+    if '_hes' in filename:
+        setattr(data, 'vib_freq', hessian(filename)[0])
+        setattr(data, 'ir_inten', hessian(filename)[1])
+
+    if '_raman' in filename:
+        setattr(data, 'raman', raman(filename))
+
+    if '_vscf' in filename:
+        setattr(data, 'vscf_freq', vscf(filename)[0])
+        setattr(data,   'vscf_ir', vscf(filename)[1])
+
+    setattr(data,  'cpu', comp(filename)[0])
+    setattr(data, 'time', comp(filename)[1])
+
+    return data
