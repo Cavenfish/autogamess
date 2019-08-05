ceff13a1204e2ec9bbdcad0255409c247a1f2cbc
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-08-05 17:02:26 -0400

    Pip install now installs dependancies too fixes #3

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Cavenfish/autogamess",
     packages=setuptools.find_packages(),
+    install_requires=["numpy", "scipy", "pandas", "basis_set_exchange", "PeriodicElements",
+                      "openpyxl"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
