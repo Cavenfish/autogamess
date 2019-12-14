8ebd0d93d8d1852deba15de461d731a3c862a383
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-12-14 17:25:45 -0500

    Version 1.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="autogamess",
-    version="1.1.8",
+    version="1.1.9",
     author="Brian C. Ferrari",
     author_email="brianf1996@knights.ucf.edu",
     description="This is a python module for automating Raman calculations using GAMESS(us).",
