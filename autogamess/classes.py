8752954d73683b96f84a9291d8124eaa18ce6638
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2020-06-14 14:15:09 -0400

    attempting new read in format, reads in fine but does not write out yet.

--- autogamess/classes.py
+++ autogamess/classes.py
@@ -1,25 +1,16 @@
 from .dictionaries import templates
-from .config       import version
+from .config       import version, read_file
 
 #Class for input parameters
 class INPUT:
     'GAMESS(US) input parameters'
 
+    #--------------------------CONTROL--------------------------------
     class Control:
         'Control class of GAMESS(US) input'
 
-        def __init__(self, params):
-            self.scftyp = params.split('SCFTYP=')[1].split()[0]
-            self.mult   = params.split('MULT='  )[1].split()[0]
-            self.nprint = params.split('NPRINT=')[1].split()[0]
-            self.coord  = params.split('COORD=' )[1].split()[0]
-            self.runtyp = params.split('RUNTYP=')[1].split()[0]
-            self.icut   = params.split('ICUT='  )[1].split()[0]
-            self.itol   = params.split('ITOL='  )[1].split()[0]
-            self.maxit  = params.split('MAXIT=' )[1].split()[0]
-            self.qmttol = params.split('QMTTOL=')[1].split()[0]
-            self.icharg = params.split('ICHARG=')[1].split()[0]
-            self.ispher = params.split('ISPHER=')[1].split()[0]
+        def __init__(self):
+            pass
 
         def make_string(self):
             s = ' $CONTROL '
@@ -36,13 +27,12 @@ class INPUT:
             s += e
             return s
 
-
+    #--------------------------SYSTEM--------------------------------
     class System:
         'System class of GAMESS(US) input'
 
-        def __init__(self, params):
-            self.mwords = params.split('MWORDS=')[1].split()[0]
-            self.memddi = params.split('MEMDDI=')[1].split()[0]
+        def __init__(self):
+            pass
 
         def make_string(self):
             s = ' $SYSTEM '
@@ -59,12 +49,12 @@ class INPUT:
             s += e
             return s
 
+    #--------------------------STATPT--------------------------------
     class Statpt:
         'Statpt class of GAMESS(US) input'
 
-        def __init__(self, params):
-            self.opttol = params.split('OPTTOL=')[1].split()[0]
-            self.nstep  = params.split('NSTEP=' )[1].split()[0]
+        def __init__(self):
+            pass
 
         def make_string(self):
             s = ' $STATPT '
@@ -81,13 +71,12 @@ class INPUT:
             s += e
             return s
 
-    class SCF:
+    #--------------------------SCF--------------------------------
+    class Scf:
         'SCF class of GAMESS(US) input'
 
-        def __init__(self, params):
-            self.dirscf = params.split('DIRSCF=')[1].split()[0]
-            self.fdiff  = params.split('FDIFF=' )[1].split()[0]
-            self.conv   = params.split('CONV='  )[1].split()[0]
+        def __init__(self):
+            pass
 
         def make_string(self):
             s = ' $SCF '
@@ -104,25 +93,81 @@ class INPUT:
             s += e
             return s
 
-    class DFT:
+    #--------------------------DFT--------------------------------
+    class Dft:
         'DFT class of GAMESS(US) input'
-        pass
 
+        def __init__(self):
+            pass
+
+        def make_string(self):
+            s = ' $DFT '
+            e = '$END\n'
+            n = len(s)
+            for i in self.__dict__:
+                add_this = i.upper() + '=' + self.__dict__[i] + ' '
+                if n + len(add_this) > 50:
+                    s += '\n '
+                    n  = 0
+                s += add_this
+                n += len(add_this)
+
+            s += e
+            return s
+
+    #--------------------------BASIS--------------------------------
     class Basis:
         'Basis class of GAMESS(US) input'
-        pass
 
+        def __init__(self):
+            pass
+
+        def make_string(self):
+            s = ' $BASIS '
+            e = '$END\n'
+            n = len(s)
+            for i in self.__dict__:
+                add_this = i.upper() + '=' + self.__dict__[i] + ' '
+                if n + len(add_this) > 50:
+                    s += '\n '
+                    n  = 0
+                s += add_this
+                n += len(add_this)
+
+            s += e
+            return s
+
+
+    #--------------------------INPUT funtions--------------------------------
     def __init__(self, template):
 
-        if '.txt' in template:
-            read_txt(template)
+        if ('.txt' in template) or ('.inp' in template):
+            self.read_txt(template)
         else:
             self.defaults(template)
 
     def read_txt(self, txtfile):
 
-        for line in read_file(txtfile):
-            line.split()
+        inp = read_file(txtfile)
+
+        inp = ''.join(inp)
+
+        inp = inp.replace('\n', '')
+
+        inp = inp.split('$END')
+
+        for i in inp:
+            if '$' not in i:
+                continue
+            i = i.split('$')[1].split()
+            group_name = i[0].capitalize()
+            params     = i[1:]
+            obj        = getattr(self, group_name)
+            for j in params:
+                j   = j.split('=')
+                key = j[0].lower()
+                val = j[1]
+                setattr(obj, key, val)
 
     def defaults(self, template):
         params       = templates[template]
@@ -144,3 +189,5 @@ class INPUT:
                 f.write(getattr(self, i).make_string())
             except:
                 continue
+
+        f.close()
