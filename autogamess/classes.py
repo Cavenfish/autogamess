2542256ecb8239495a5cd7b6d2070fac807e7284
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2020-06-14 22:06:12 -0400

    this is a much better structure design, still needs to have  integrated

--- autogamess/classes.py
+++ autogamess/classes.py
@@ -5,18 +5,19 @@ from .config       import version, read_file
 class INPUT:
     'GAMESS(US) input parameters'
 
-    #--------------------------CONTROL--------------------------------
-    class Control:
-        'Control class of GAMESS(US) input'
+    class Param_Group:
+        'Subclass for GAMESS(US) input parameter groups'
 
-        def __init__(self):
-            pass
+        def __init__(self, name):
+            self.name = name
 
         def make_string(self):
-            s = ' $CONTROL '
+            s = ' $' + self.name.upper() + ' '
             e = '$END\n'
             n = len(s)
             for i in self.__dict__:
+                if i is 'name':
+                    continue
                 add_this = i.upper() + '=' + self.__dict__[i] + ' '
                 if n + len(add_this) > 50:
                     s += '\n '
@@ -26,117 +27,6 @@ class INPUT:
 
             s += e
             return s
-
-    #--------------------------SYSTEM--------------------------------
-    class System:
-        'System class of GAMESS(US) input'
-
-        def __init__(self):
-            pass
-
-        def make_string(self):
-            s = ' $SYSTEM '
-            e = '$END\n'
-            n = len(s)
-            for i in self.__dict__:
-                add_this = i.upper() + '=' + self.__dict__[i] + ' '
-                if n + len(add_this) > 50:
-                    s += '\n '
-                    n  = 0
-                s += add_this
-                n += len(add_this)
-
-            s += e
-            return s
-
-    #--------------------------STATPT--------------------------------
-    class Statpt:
-        'Statpt class of GAMESS(US) input'
-
-        def __init__(self):
-            pass
-
-        def make_string(self):
-            s = ' $STATPT '
-            e = '$END\n'
-            n = len(s)
-            for i in self.__dict__:
-                add_this = i.upper() + '=' + self.__dict__[i] + ' '
-                if n + len(add_this) > 50:
-                    s += '\n '
-                    n  = 0
-                s += add_this
-                n += len(add_this)
-
-            s += e
-            return s
-
-    #--------------------------SCF--------------------------------
-    class Scf:
-        'SCF class of GAMESS(US) input'
-
-        def __init__(self):
-            pass
-
-        def make_string(self):
-            s = ' $SCF '
-            e = '$END\n'
-            n = len(s)
-            for i in self.__dict__:
-                add_this = i.upper() + '=' + self.__dict__[i] + ' '
-                if n + len(add_this) > 50:
-                    s += '\n '
-                    n  = 0
-                s += add_this
-                n += len(add_this)
-
-            s += e
-            return s
-
-    #--------------------------DFT--------------------------------
-    class Dft:
-        'DFT class of GAMESS(US) input'
-
-        def __init__(self):
-            pass
-
-        def make_string(self):
-            s = ' $DFT '
-            e = '$END\n'
-            n = len(s)
-            for i in self.__dict__:
-                add_this = i.upper() + '=' + self.__dict__[i] + ' '
-                if n + len(add_this) > 50:
-                    s += '\n '
-                    n  = 0
-                s += add_this
-                n += len(add_this)
-
-            s += e
-            return s
-
-    #--------------------------BASIS--------------------------------
-    class Basis:
-        'Basis class of GAMESS(US) input'
-
-        def __init__(self):
-            pass
-
-        def make_string(self):
-            s = ' $BASIS '
-            e = '$END\n'
-            n = len(s)
-            for i in self.__dict__:
-                add_this = i.upper() + '=' + self.__dict__[i] + ' '
-                if n + len(add_this) > 50:
-                    s += '\n '
-                    n  = 0
-                s += add_this
-                n += len(add_this)
-
-            s += e
-            return s
-
 
     #--------------------------INPUT funtions--------------------------------
     def __init__(self, template):
@@ -159,15 +49,15 @@ class INPUT:
         for i in inp:
             if '$' not in i:
                 continue
-            i = i.split('$')[1].split()
-            group_name = i[0].capitalize()
-            params     = i[1:]
-            obj        = getattr(self, group_name)
+            i                             = i.split('$')[1].split()
+            group_name                    = i[0].capitalize()
+            params                        = i[1:]
+            exec('self.' + group_name + ' = self.Param_Group(group_name)')
             for j in params:
                 j   = j.split('=')
                 key = j[0].lower()
                 val = j[1]
-                setattr(obj, key, val)
+                exec('setattr(self.' + group_name + ', key, val)')
 
     def defaults(self, template):
         params       = templates[template]
