d1ffab756602ad5cb93610e919883150ebd9f3cd
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2020-06-26 15:11:38 -0400

    working well, needs minor tweaks for better user control over style preferences

--- autogamess/plots/make_scatter.py
+++ autogamess/plots/make_scatter.py
@@ -1,6 +1,6 @@
 from ..config import *
 import matplotlib.pyplot as plt
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, linregress
 
 def make_scatter(data, savedir):
     """
@@ -29,9 +29,7 @@ def make_scatter(data, savedir):
     X        = 'X'
 
     #Predefine Markers and Colors
-    markers  = ['x', '<', '>', 's', 'o', 'd', '*', '$\odot$', '$\dagger$',
-                '$\oplus$', '$\circledcirc$', '$\oslash$',
-                '$\ltimes$']
+    markers  = ['X', '<', '>', 's', 'o', 'd', '*', '^', 'P', 'p']
     colors   = ['r','k','blue','darkorange', 'lime','olive', 'deepskyblue',
                 'purple', 'gold']
 
@@ -54,8 +52,11 @@ def make_scatter(data, savedir):
             y   = df[col]
             l   = np.arange(min(x), max(x), (max(x) - min(x))/100)
             try:
-                t,p = ttest_ind(x, y, equal_var=False, nan_policy='omit')
-                s   = 't = ' + str(round(t, 3)) + '\np = ' + str(round(p, 3))
+                mask = ~np.isnan(x) & ~np.isnan(y)
+                m, inter, r, p_val, sig = linregress(x[mask], y[mask])
+                t,p = ttest_ind(x[mask], y[mask], equal_var=False)
+                s   = 't = ' + str(round(t, 3)) + '\np = ' + str(round(p, 3)) +\
+                      '\nr² = ' + str(round(r**2, 3))
             except:
                 s   = ''
 
@@ -64,14 +65,15 @@ def make_scatter(data, savedir):
             plt.scatter(x, y, marker='x', color='red', label=col)
             plt.plot(l, l, color='black', linewidth=0.75, label=None)
             if s:
-                txt = plt.text(0.05, 0.85, s, transform=ax.transAxes)
+                txt = plt.text(0.05, 0.8, s, transform=ax.transAxes)
             plt.legend()
             plt.tight_layout()
             plt.savefig(dir_path + 'Linear.png')
             if 'Bond' in name:
                 plt.close()
             else:
-                plt.loglog()
+                plt.yscale('symlog')
+                plt.xscale('symlog')
                 plt.savefig(dir_path + 'LogLog.png')
                 plt.close()
 #--------------------Original Plot----------------------------------
@@ -86,9 +88,59 @@ def make_scatter(data, savedir):
             if 'Bond' in name:
                 plt.close()
             else:
-                plt.loglog()
+                plt.yscale('symlog')
+                plt.xscale('symlog')
                 plt.savefig(dir_path + 'LogLog--Dif.png')
                 plt.close()
 #--------------------Difference Plot----------------------------------
+
+
+#--------------------Combination Plot----------------------------------
+        i=0
+        dir_path = savedir + name + '/'
+        fig, ax = plt.subplots()
+        for col in df:
+            if col == ref:
+                continue
+            df.plot.scatter(ref, col, marker=markers[i], edgecolor=colors[i],
+                            label=col, ax=ax, c='none')
+            i += 1
+        plt.plot(l, l, color='black', linewidth=0.75, label=None)
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(dir_path + 'Linear--All.png')
+        if 'Bond' in name:
+            plt.close()
+        else:
+            plt.yscale('symlog')
+            plt.xscale('symlog')
+            plt.savefig(dir_path + 'LogLog--All.png')
+            plt.close()
+#--------------------Combination Plot----------------------------------
+
+
+#--------------------Combination Diff Plot----------------------------------
+        i=0
+        dir_path = savedir + name + '/'
+        fig, ax = plt.subplots()
+        for col in df:
+            if col == ref:
+                continue
+            x   = df[ref]
+            y   = df[col]
+            plt.scatter(x, y-x, marker=markers[i], edgecolor=colors[i],
+                            label=False, c='none')
+            i += 1
+        plt.plot(l, l-l, color='black', linewidth=0.75, label=None)
+        plt.tight_layout()
+        plt.savefig(dir_path + 'Linear--Diff--All.png')
+        if 'Bond' in name:
+            plt.close()
+        else:
+            plt.yscale('symlog')
+            plt.xscale('symlog')
+            plt.savefig(dir_path + 'LogLog--Diff--All.png')
+            plt.close()
+#--------------------Combination Diff Plot----------------------------------
 
     return
