0da62f43afded16689401153953b2ebe0808e2ad
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2020-06-26 16:55:02 -0400

    Full KDE plot works, subplots doesnt work yet

--- autogamess/plots/make_histogram.py
+++ autogamess/plots/make_histogram.py
@@ -1,15 +1,9 @@
 from ..config import *
 import matplotlib.pyplot as plt
+from ..dictionaries  import theory_dict
 
-def make_histogram(sheetsdir, ref_theory, savedir):
+def make_histogram(data, savedir):
     """
-    sheetsdir: string
-        This should be a full directory string pointing to the spreadsheets
-        directory.
-
-    savedir: string
-        This should be a string that points to the directory in which you
-        would like to save the png of the plot.(FULL DIRECTORY STRING REQUIRED)
     """
     #Define Sheet names
     opt = 'Optimization'
@@ -17,81 +11,74 @@ def make_histogram(sheetsdir, ref_theory, savedir):
     ram = 'Raman'
     vsc = 'VSCF'
 
-    #Define Data Column names
+    #Define Data Column names/variables
     rt = 'Run Time'
     bs = 'Basis Set'
     cp = 'CPU Percentage'
     te = 'Theory'
     me = 'Method'
+    fe = 'Freq'
+    bl = 'Bond Length'
+    ir = 'Infrared'
+    ra = 'Raman'
 
     #Other predefined strings
-    xlsx = '.xlsx'
+    png      = '.png'
+    xlsx     = '.xlsx'
     engine   = 'xlsxwriter'
+    X        = 'X'
 
-    #Create a DataFrame for storing the plottable data
-    to_plot = pd.DataFrame()
+    #Predefine Markers and Colors
+    colors   = ['r','k','blue','darkorange', 'lime','olive', 'deepskyblue',
+                'purple', 'gold']
 
-    #iterate through files in sheetsdir
-    for file in os.listdir(sheetsdir):
 
-        #Make DataFrame from the excel file
-        df = pd.read_excel(sheetsdir + file, index_col=0,
-                           sheet_name=opt, header=6)
+    if xlsx in data:
+        data = pd.read_excel(data,  index_col=0, sheet_name=None, header=6)
 
-        #Pull the subset of the Data Frame that is
-        #reference theory level data
-        ref_df = df[ df[te] == ref_theory[0] ]
-        ref_df = ref_df[ ref_df[bs] == ref_theory[1] ]
+    for name, df in data.items():
+        ref = df.columns[0]
+        dir_path = savedir + name + '/'
 
-        #dummy DataFram for holding subrated data
-        dum = df.copy()
-
-        #iterate through DataFrame rows
-        #Note ->j<- is the row in pandas series format
-        #Note ->i<- is the index of the row
-        for i,j in df.iterrows():
-
-            #pull out the current row from the DataFrame
-            #note, it is pulled out in DataFrame format
-            data = df.loc[i:i]
-
-            #pull out the reference theory data in Series format
-            x   = ref_df.index[0]
-            ref = ref_df.loc[x]
-
-            #Subtracts the reference theory data from the current row
-            #Note this only performes the subtration on frequencies
-            tmp = data.filter(regex='Length') - ref.filter(regex='Length')
-            dum.update(tmp)
-
-        #This puts the DataFrame in a better format for plotting
-        dum = dum.set_index(te).transpose().filter(regex='Length', axis=0)
-
-        #This updates the plotting DataFrame
-        to_plot = to_plot.append(dum)
-
-    to_plot.dropna(thresh=6, axis='columns', inplace=True)
-    writer = pd.ExcelWriter('../../to_plot3.xlsx', engine=engine)
-    to_plot.to_excel(writer, startrow=0, startcol=0)
-    writer.save()
-    #This will take a DataFrame with data in the normal format from
-    #AutoGAMESS and plot a histogram of the data with theory levels
-    #as the labels (exactly as wanted)
-    """
-    to_plot.dropna(thresh=5,axis='columns').plot.kde()
-    plt.tight_layout()
-    plt.savefig(savedir+'test.png')
-    plt.close()
-
-    #ax=plt.figure()
-    for label, sub_df in to_plot.transpose().groupby(te):
-        sub_df.dropna(thresh=5, inplace=True)
-        x = sub_df.values.astype(np.float64)
         try:
-            plt.kde()
+            os.makedirs(dir_path)
         except:
-            ax = plt.kde()
-    plt.tight_layout()
-    plt.savefig(savedir+'test2.png')
-    plt.close()
-    """
+            pass
+
+        for col in df:
+            if col == ref:
+                continue
+            x = df[ref]
+            y = df[col]
+            df[col] = y-x
+
+
+        df.drop(ref, axis=1, inplace=True)
+
+        df.dropna(thresh=5, axis=1).plot.kde(
+        color=('r','k','blue','darkorange', 'lime','olive', 'deepskyblue',
+               'purple', 'gold'),
+        style=['-', '-', '-', '-', '-', '-', '-', '--', '-'])
+        plt.tight_layout()
+        plt.savefig(dir_path + 'KDE.png')
+        plt.close()
+
+        dft = [k for k,v in theory_dict.items() if 'DFTTYP' in v]
+        mp2 = [k for k,v in theory_dict.items() if 'MPLEVL' in v]
+        cc  = [k for k,v in theory_dict.items() if 'CCTYP' in v]
+
+        ax = plt.subplot(2,1, 1)
+        df.filter(regex=dft).plot.kde(color=colors[mask], ax=ax, sharex=True,
+                                      legend=False, style=styles[mask])
+        plt.xlim(-0.075,0.075)
+        plt.ylabel('')
+        #plt.title(r'Vibrational Frequencies Discrepancies', size=18)
+        ax = plt.subplot(2,1, 2)
+        df.filter(regex=mp2+cc).plot.kde(color=colors[mask], ax=ax, sharex=True,
+                                         legend=False, style=styles[mask])
+        plt.xlim(-0.075,0.075)
+        plt.ylabel('')
+        plt.xlabel(r'Predicted Bond Length Difference ($\AA$)', size=15)
+        plt.tight_layout()
+        plt.savefig('plot_with_subplots-opt.png')
+        plt.close()
