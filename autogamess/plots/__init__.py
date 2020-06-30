f624666ca0c95dc31f404365a3a5ddc2e3b384e2
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2020-06-29 20:40:38 -0400

    forgot to change the histogram to kde in here

--- autogamess/plots/__init__.py
+++ autogamess/plots/__init__.py
@@ -7,6 +7,6 @@ Author: Brian C. Ferrari
 from ..config                  import *
 
 
-from .make_histogram           import make_histogram
+from .make_kde                 import make_kde
 from .make_scatter             import make_scatter
 from .prep_data                import prep_data
